8ada0dc00da94ba63c62780b30a7a7fb8dd6d536
Layla Rodas <rodas.layla@gmail.com>
2024-04-12 20:11:01 +0200

    creating form -- HomePage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,48 @@
-
+import { useMemo } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export const Header = () => {
+
+    const location = useLocation();
+
+    const isHome = useMemo(() => location.pathname === '/', [location.pathname]);
+
+
     return (
         <header className="bg-slate-800">
-            <div className="mx-auto container px-5 py-16">
+            <div className="mx-auto container px-14 py-16">
                 <div className="flex justify-between items-center">
                     <div>
                         <img src="/logo.svg" alt="logo" className="w-32" />
                     </div>
-                    <nav>
-                        
-                    </nav>   
-                </div>         
+                    <nav className='flex gap-4'>
+                        <NavLink to="/" className={({ isActive }) => isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'}>Home</NavLink>
+                        <NavLink to="/favourites" className={({ isActive }) => isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'}>Favourites</NavLink>
+                    </nav>
+                </div>
+                {isHome && (
+                    <form action="" className='md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6'>
+                        <div className='space-y-4'>
+                            <label htmlFor="ingredient" className=' block text-white uppercase font-extrabold text-lg'>
+                                Ingredients
+                            </label>
+                            <input type="text" id='ingredient' name='ingredient'
+                                className='p-3 w-full rounded-lg focus:outline-none'
+                                placeholder='Enter an ingredient. Ej: vodka, tequila, etc.'
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="" className=' block text-white uppercase font-extrabold text-lg'>
+                                Category
+                            </label>
+
+                            <select name="category" id="category" className='p-3 w-full rounded-lg focus:outline-none'>
+                                <option value="">--- Select ---</option>
+                            </select>
+                        </div>
+                        <input type="submit" value='Search Recipes' className='cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-bold w-full p-2 rounded-lg uppercase'/>
+                    </form>
+                )}
             </div>
         </header>
     )
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom"
 import { Header } from "../components/Header"
 
 export const Layout = () => {
-  return (
-    <>
-        <Header />
-        <Outlet />
-    </>
-   
-  )
+    return (
+        <>
+            <Header />
+            <main className="container mx-auto p-16">
+                <Outlet />
+            </main>
+        </>
+
+    )
 }
