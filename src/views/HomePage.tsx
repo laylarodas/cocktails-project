05ffcc22a48f6976af2ad205924cd27e3a4847f5
Layla Rodas <rodas.layla@gmail.com>
2024-04-13 21:22:35 +0200

    styles drink card

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -14,14 +14,14 @@ export const HomePage = () => {
         <h1 className=" text-4xl font-bold">Recipes</h1>
 
         {hasDrinks ? (
-          <>
+          <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 mx-16 my-20 gap-12">
             {drinks.drinks.map((drink) => (
                 <DrinkCard 
                     key={drink.idDrink}
                     drink={drink}
                 />
             ))}
-          </>
+          </div>
         ) : (
             <p className=" my-10 text-center text-2xl">There are no drink. Use the form to get recipes </p>
    
